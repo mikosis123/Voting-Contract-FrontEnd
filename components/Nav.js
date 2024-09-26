130ec6597d3712dfbedadaa95bd9d64897f3e8b6
mikosis123 <mikosis93@gmail.com>
2024-09-26 14:38:53 +0300

    nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,7 @@ import { ConnectButton } from "thirdweb/react";
 
 const Nav = () => {
   return (
-    <div className="w-full h-24 bg-blue-800 flex justify-between item-center">
+    <div className="w-full h-24 bg-blue-700 flex justify-between item-center">
       <h1 className="text-3xl text-white p-6">Voting Contract</h1>
       <div className="my-auto p-6">
         <ConnectButton
