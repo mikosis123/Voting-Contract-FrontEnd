586384019be748b628b08f791c5d8fcb38394d14
mikosis123 <mikosis93@gmail.com>
2024-09-26 13:53:01 +0300

    nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -7,14 +7,14 @@ const Nav = () => {
   return (
     <div className="w-full h-24 bg-blue-800 flex justify-between item-center">
       <h1 className="text-3xl text-white p-6">Voting Contract</h1>
-      <div>
+      <div className="my-auto p-6">
         <ConnectButton
           connectButton={{
             label: "Connect your wallet",
             className: "my-custom-class",
             style: {
               borderRadius: "10px",
-              marginTop: "20px",
+              paddingTop: "20px",
             },
           }}
           client={client}
