39cc8082d357fa9c03c93442b4866123e48bb256
mikosis123 <mikosis93@gmail.com>
2024-09-26 13:15:43 +0300

    wrapping the app with thirdweb provider

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThirdwebProvider>{children}</ThirdwebProvider>
       </body>
     </html>
   );
